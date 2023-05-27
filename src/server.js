616662d93d9dd3b75d8080642f9df868acc6a927
Neander de Souza Faria <neander.faria@hotmail.com>
2023-05-26 22:05:39 -0300

    node --watch

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 
 const server = http.createServer((request, response) => {
-  return response.end('Hello Word')
+  return response.end('Hello Ignite')
 })
 
 server.listen(3333)
