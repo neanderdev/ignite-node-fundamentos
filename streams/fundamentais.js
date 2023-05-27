678844738238010dd0bf941de62dfb8cdaeb2965
Neander de Souza Faria <neander.faria@hotmail.com>
2023-05-26 22:34:26 -0300

    Entendendo Streams no Node

---
+++ streams/fundamentais.js
@@ -0,0 +1,15 @@
+// Exemplo usando - Writable Streams
+// Netflix && Spotify
+
+// Exemplo usando - Readable Streams
+// Importação de clientes via CSV (Excel)
+// 1gb - 1.000.000
+// POST /upload import.csv
+
+// 10mb/s - 100s
+
+// 100s -> Inserções no banco de dados
+
+// 10mb/s -> 10.000 linhas
+
+// Readable Streams / Writable Streams
