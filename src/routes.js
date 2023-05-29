8bb3522d5aecfb08b54943a2f6d8c81e22ebba88
Neander de Souza <neander.faria@hotmail.com>
2023-05-29 10:16:37 -0300

    Separando rotas da aplicação

---
+++ src/routes.js
@@ -0,0 +1,35 @@
+import { randomUUID } from 'node:crypto'
+
+import { Database } from './database.js'
+
+const databse = new Database()
+
+export const routes = [
+    {
+        method: 'GET',
+        path: '/users',
+        handler: (request, response) => {
+            const users = databse.select('users')
+
+            return response
+            .end(JSON.stringify(users))
+        }
+    },
+    {
+        method: 'POST',
+        path: '/users',
+        handler: (request, response) => {
+            const { name, email } = request.body
+
+            const user = {
+                id: randomUUID(),
+                name,
+                email,
+            }
+
+            databse.insert('users', user)
+
+            return response.writeHead(201).end()
+        }
+    },
+]
